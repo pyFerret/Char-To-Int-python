ad8fedb2961c7040c88234293fdc0ae670d251c6
pyFerret <69222489+pyFerret@users.noreply.github.com>
2020-10-13 22:18:12 -0500

    Update cti.py
    Adds "pretty" feature to the multi function

--- cti.py
+++ cti.py
@@ -2,28 +2,35 @@ letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
 
 
 def single(_conv): # takes single character. will return 0 if given more
-    result = 0
+  result = 0
+  for i in range(len(letters)):
+    if (str(_conv) == letters[i]):
+      if (i >= 27):
+        result = i + 76
+      else:
+        result = i + 65
+    if (result == 128):
+      result = 32
+  return result # returns int. will not print result
+
+def multi(_conv, _pretty): # takes any amount of characters
+  result = []
+  for a in range(len(_conv)):
     for i in range(len(letters)):
-        if (str(_conv) == letters[i]):
-            if (i >= 27):
-                result = i + 76
-            else:
-                result = i + 65
-        if (result == 128):
-          result = 32
-    return result # returns int. will not print result
-
-def multi(_conv): # takes any amount of characters
-    result = []
-    for a in range(len(_conv)):
-        for i in range(len(letters)):
-            if (str(_conv[a]) == letters[i]):
-                if (i >= 26):
-                    if (i < 52):
-                        result.append(i + 71)
-                    else:
-                        result.append(32)
-                else:
-                    result.append(i + 65)
-
-    return result # returns list. will not print result
+      if (str(_conv[a]) == letters[i]):
+        if (i >= 26):
+          if (i < 52):
+            result.append(i + 71)
+          else:
+            result.append(32)
+          else:
+            result.append(i + 65)
+  if(bool(_pretty) == True):
+    newResult = ""
+    for i in range(len(result)):
+      newResult += str(result[i])
+      if(i != len(result) - 1):
+        newResult += ", "
+        return newResult # returns string. will not print result
+      else:
+        return result # returns list. will not print result
