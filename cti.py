09b6a5a371bd77f3f4cf1ad9b68de645087ac2b8
pyFerret <69222489+pyFerret@users.noreply.github.com>
2020-10-15 01:26:02 -0500

    Major Code optimizations
    added modularity to the data, changed some variables, and optimized much of the code to run faster

--- cti.py
+++ cti.py
@@ -1,52 +1,37 @@
-letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O","P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d","e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s","t", "u", "v", "w", "x", "y", "z", " "]
+data = [["A", 65],["B", 66],["C", 67],["D", 68],["E", 69],["F", 70],["G", 71],["H", 72],["I", 73],["J", 74],["K", 75],["L", 76],["M", 77],["N", 78],["O", 79],["P", 80],["Q", 81],["R", 82],["S", 83],["T", 84],["U", 85],["V", 86],["W", 87],["X", 88],["Y", 89],["Z", 90],["a", 97],["b", 98],["c", 99],["d", 100],["e", 101],["f", 102],["g", 103],["h", 104],["i", 105],["j", 106],["k", 107],["l", 108],["m", 109],["n", 110],["o", 111],["p", 112],["q", 113],["r", 114],["s", 115],["t", 116],["u", 117],["v", 118],["w", 119],["x", 120],["y", 121],["z", 122],[" ", 32],[",", 44],[".", 46],["!", 33],["?", 63]]
 
+def single(_conv):
+    result = 0
+    for i in range(len(data)):
+        if (str(_conv) == data[i][0]):
+            result = data[i][1]
+    return result
 
-def single(_conv): # takes single character. will return 0 if given more
-  result = 0
-  for i in range(len(letters)):
-    if (str(_conv) == letters[i]):
-      if (i >= 27):
-        result = i + 76
-      else:
-        result = i + 65
-    if (result == 128):
-      result = 32
-  return result # returns int. will not print result
+def multi(_conv, _pretty = False):
+    result = []
+    for a in range(len(_conv)):
+        for i in range(len(data)):
+            if (str(_conv[a]) == data[i][0]):
+              result.append(data[i][1])
 
-def multi(_conv, _pretty = False): # takes any amount of characters
-  result = []
-  for a in range(len(_conv)):
-    for i in range(len(letters)):
-      if (str(_conv[a]) == letters[i]):
-        if (i >= 26):
-          if (i < 52):
-            result.append(i + 71)
-          else:
-            result.append(32)
-          else:
-            result.append(i + 65)
-  if(bool(_pretty) == True):
-    newResult = ""
-    for i in range(len(result)):
-      newResult += str(result[i])
-      if(i != len(result) - 1):
-        newResult += ", "
-    return newResult # returns string. will not print result
-  else:
-    return result # returns list. will not print result
+    if(bool(_pretty) == True):
+      newResult = ""
+      for i in range(len(result)):
+        newResult += str(result[i])
+        if(i != len(result) - 1):
+          newResult += ", "
+      
+      return newResult 
+    else:
+      return result 
 
 def prtMulti(_conv):
   result = []
   for a in range(len(_conv)):
-    for i in range(len(letters)):
-      if (str(_conv[a]) == letters[i]):
-        if (i >= 26):
-          if (i < 52):
-            result.append(i + 71)
-          else:
-            result.append(32)
-        else:
-          result.append(i + 65)
+    for i in range(len(data)):
+      if (str(_conv[a]) == data[i][0]):
+        result.append(data[i][1])
+
 
   strResult = ""
   for i in range(len(result)):
